43853d905a105ba13cf28988b7f7974e95ed54a5
MesfinGithinji <meshgithinji@gmail.com>
2023-12-08 00:54:27 +0300

    use of str function to return a string representation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""A function that returns a string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """Returns a string representation of a float
+    Args:
+        n: The float to convert to string
+    Returns:
+        String representation of the float
+    """
+    return str(n)
