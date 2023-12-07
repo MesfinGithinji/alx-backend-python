24b93f8ea7f1fa2799d085a628be19116a205e26
MesfinGithinji <meshgithinji@gmail.com>
2023-12-08 00:52:42 +0300

    More on type annotated functions plus the return type

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A function that calculates the floor of a number."""
+import math
+
+
+def floor(n: float) -> int:
+    """Return floor of the float."""
+    return math.floor(n)
